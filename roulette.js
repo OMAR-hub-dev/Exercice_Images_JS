04493302e10daa4283c00430a67ee975da41dcf7
Oulaya omar <oulaya.omar@gmail.com>
2024-04-23 15:52:58 +0200

    version final

--- roulette.js
+++ roulette.js
@@ -2,11 +2,14 @@ const lancer = document.querySelector('button');
 const circles = document.querySelectorAll('.circles');
 const counter = document.querySelector('.counter');
 const somme = document.querySelector('.somme');
+const titre = document.querySelector('h1');
+const nouveau = document.querySelector('.nouveau')
 let choix = 0;
 let tir =0;
+let total= 0;
 
 
-
+// **********  gérer les choix de boutons rouges   *****************************
 circles.forEach((circle)=>{
     circle.addEventListener('click',()=>{
         if(choix <3) {
@@ -15,45 +18,41 @@ circles.forEach((circle)=>{
         }
     })
 });
+// **********  gérer les choix de bouton Nouveau jeu   *****************************
+nouveau.addEventListener('click',()=>{
+    circles.forEach((circle)=>{circle.style.fill = 'gray'});
+    choix =0;
+    tir = 0;
+    counter.innerHTML = 0;
+    titre.textContent = "Roulette Russe";
+    titre.classList.remove('vainqueur');
+
+    
+});
+
+// **********  gérer les choix de bouton jouer   *****************************
 
 lancer.addEventListener('click', ()=>{
     // console.log('ok');
     const valeur = Math.floor((Math.random() * 6)+1);
-    circles.forEach((circle)=>{
-        if(circle.style.fill !== 'red'){
-            circle.style.fill = 'gray';
-        }
-    });
+        
     if (tir < 3){
         tir++; 
-        counter.innerHTML = tir;      
-        switch (valeur) {
-            case 1:
-                document.getElementById('C3').style.fill = 'blue';            
-                break;
-            case 2:
-                document.getElementById('C4').style.fill = 'blue';
-                break;
-            case 3:
-                document.getElementById('C5').style.fill = 'blue';
-                break;
-            case 4:
-                document.getElementById('C6').style.fill = 'blue';
-                break;
-            case 5:
-                document.getElementById('C7').style.fill = 'blue';
-                break;
-            case 6:
-                document.getElementById('C8').style.fill = 'blue';
-                break;
-        }
+        counter.innerHTML = tir; 
+        if(document.getElementById(`C${valeur}`).style.fill == 'red' ){ 
+            titre.textContent = "bravo vous avez gagné !!!";
+            titre.classList.add('vainqueur')           
+            document.getElementById(`C${valeur}`).style.fill = 'blue';
+            
+            if (choix ==1) {  total += 10000;      }           
+            if (choix ==2) {  total += 100000;    }           
+            if (choix ==3) {  total += 1000000;    } 
+            // somme.textContent = total ;
+            somme.textContent = total        
+        }else{document.getElementById(`C${valeur}`).style.fill = 'blue';}
+      
     }
-    // if(document.getElementById('C8').style.fill == 'blue')
-    // {somme.textContent = "10.000 €"}
-    // if((document.getElementById('C8').style.fill = 'red') && (document.getElementById('C5').style.fill = 'red'))
-    // {somme.textContent = "100.000 €"}
-    // if((document.getElementById('C8').style.fill = 'red') && (document.getElementById('C5').style.fill = 'red') && (document.getElementById('C4').style.fill = 'red'))
-    // {somme.textContent = "1.000.000 €"}
+    
 });
 
 
