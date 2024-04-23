c39b5f7d3e4b58b134c72a5b1d7d84a89baf70e8
Oulaya omar <oulaya.omar@gmail.com>
2024-04-23 11:04:13 +0200

    roulette_counter

--- roulette.js
+++ roulette.js
@@ -1,12 +1,16 @@
 const lancer = document.querySelector('button');
 const cirles = document.querySelectorAll('.circles');
+const counter = document.querySelector('.counter');
+let tir = 0;
 
 lancer.addEventListener('click', ()=>{
+    tir++;
+    counter.innerHTML = tir;
     const valeur = Math.floor((Math.random() * 6)+1);
     cirles.forEach(x=>x.style.fill ='gray');
     switch (valeur) {
         case 1:
-            document.getElementById('C3').style.fill = 'red';
+            document.getElementById('C3').style.fill = 'red';            
             break;
         case 2:
             document.getElementById('C4').style.fill = 'red';
